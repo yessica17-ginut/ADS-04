d533bee7cf50adc77055db0a6386bc805d430239
yessica17 <yessicapalacios2025@gmail.com>
2025-10-16 16:47:31 -0500

    ejercicio 17

---
+++ Ejercicio17/src/main/java/com/sena/ejercicio17/Ejercicio17.java
@@ -0,0 +1,9 @@
+public class Ejercicio17 {
+static Scanner dato = new Scanner(System.in):
+
+    public static void main(String[] args) {
+       String sexo,consul;
+       int clin,edad;
+       while (al ingresar un numero de historia clinica = a -1 terminara el proceso)
+    }
+}
